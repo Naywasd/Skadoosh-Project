0cfbccc909a6be068aefdaf7fdd2e34df4d74f3b
kennumonenumone-cell <kennumonenumone@gmail.com>
2026-02-18 22:46:36 +0700

    commit add backend.cpp

--- backend.cpp
+++ backend.cpp
@@ -10,11 +10,19 @@ int toMinutes(int h, int m) {  // แปลงชมเป็นนาที
 void fillSchedule(int dIdx, string name, int sH, int sM, int eH, int eM) {
     int start = toMinutes(sH, sM); // เวลาที่เรื่มทำรูทีน
     int end = toMinutes(eH, eM); // เวลาจบรูทีน
+
+    if (end < start) { 
+        for(int m = start; m < 1440; m++) week[dIdx].timeSlots[m] = name;
+        for(int m = 0; m <= end; m++) week[dIdx].timeSlots[m] = name;
+    } else {
+        for(int m = start; m <= end; m++) week[dIdx].timeSlots[m] = name;
+    }
+}
    
 
 
 
 
-}
+
 
 
