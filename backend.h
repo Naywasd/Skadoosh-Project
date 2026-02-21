d42d8ab14202924a4689d824c52aac686d8c8fa3
kennumonenumone-cell <kennumonenumone@gmail.com>
2026-02-22 00:20:38 +0700

    backend ต่อ

---
+++ backend.h
@@ -0,0 +1,12 @@
+#ifndef BACKEND_H
+#define BACKEND_H
+
+#include <string>
+
+using namespace std;
+
+int toMinutes(int h, int m);
+void fillSchedule(int dIdx, string name, int sH, int sM, int eH, int eM);
+int getNetFreeMinutes(int dDay, int dHour, int dMinute);
+
+#endif
