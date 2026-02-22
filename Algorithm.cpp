3f0a78b860fb7efe23f28b2441b879748d549417
PhanupongWOwwww <zozackgt@gmail.com>
2026-02-22 18:02:16 +0700

    Algorithm aaa

---
+++ Algorithm.cpp
@@ -0,0 +1,14 @@
+#include "Algorithm.h"
+#include "Data.h"
+#include "Backend.h" // ต้องใช้ getNetFreeMinutes
+
+void processTaskPriority() {
+    for (auto& t : taskList) {
+        int free = getNetFreeMinutes(t.deadlineDay, t.deadlineHour, t.deadlineMinute);
+        t.stressIndex = (free <= 0) ? 999.0 : (double)t.remainingMinutes / free; //คำนวณความรีบของงาน
+    }
+
+    sort(taskList.begin(), taskList.end(), [](const Task& a, const Task& b) {
+        return a.stressIndex > b.stressIndex;//จัดลำดับความสำคัญ
+    });
+}
